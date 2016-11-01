1ef05d73c0ade36378b7f4634a29d90d8bb34440
Ken Kundert <thenurd@nurdletech.com>
2016-11-01 15:36:44 -0700

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='shlib',
-    version='0.2.0',
+    version='0.3.0',
     description='shell library',
     long_description=readme,
     author="Ken Kundert",
