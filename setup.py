b25609422e1d51212f5982ea51d3cbe54c96c090
Ken Kundert <ken@theKunderts.net>
2020-11-06 21:00:23 -0800

    update list of supported python versions

--- setup.py
+++ setup.py
@@ -32,15 +32,10 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.7",
-        # 'Programming Language :: Python :: 3.3',
-        #     should work, but is no longer tested
-        # 'Programming Language :: Python :: 3.4',
-        #     should work, but is no longer tested
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Utilities",
     ],
 )
